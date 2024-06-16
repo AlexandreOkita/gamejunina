0498b23d963b9eb4f594fd817624310d16c28857
Luiz Alves (Hik) <luizhenrique98@gmail.com>
2024-06-16 17:42:12 -0300

    feat: damage color

--- Assets/Scripts/DamageFeedback.cs
+++ Assets/Scripts/DamageFeedback.cs
@@ -11,6 +11,7 @@ namespace DefaultNamespace
         [SerializeField] SpriteRenderer _mainRenderer;
         [SerializeField] Color _damageColor;
         [SerializeField, Range(0, 4f)] float _effectDuration;
+        [SerializeField] PlayerController _playerController;
 
         void Start()
         {
@@ -20,7 +21,7 @@ namespace DefaultNamespace
         void ShowFeedback(float damage)
         {
             var feedback = Instantiate(_damagePrefab, transform.position, Quaternion.identity);
-            feedback.Setup(damage);
+            feedback.Setup(damage, _playerController == null ? null : _playerController.PlayerColor);
             TweenParams tParms = new TweenParams().SetLoops(2).SetEase(Ease.Flash);
             _mainRenderer.DOColor(_damageColor, _effectDuration).SetAs(tParms).OnComplete(() => _mainRenderer.color = Color.white);
         }
